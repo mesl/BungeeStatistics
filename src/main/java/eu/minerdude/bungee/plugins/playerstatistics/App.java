cc852aaf84459be7fab349bade052c7594819fbc
Merlyn van der Sluis <Merlyn van der Sluis>
2017-12-25 17:34:59 +0100

    Added first sources

---
+++ src/main/java/eu/minerdude/bungee/plugins/playerstatistics/App.java
@@ -0,0 +1,13 @@
+package eu.minerdude.bungee.plugins.playerstatistics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
